f45ffd35f85e232a37431f02d5abc140891036db
Dhruv Saini <me2201014@iitd.ac.in>
2021-07-23 02:22:29 +0530

    introduced todos content

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from "./Mycomponents/Header";
+import Header from './Mycomponents/Header';
+import { Todos } from "./Mycomponents/Todos";
+import { Footer } from "./Mycomponents/Footer";
 
 function App() {
-  
+  const onDelete = ()=> {
+    console.log("I am onDelete");
+  }
+  let todos = [
+    {
+      sno: 1,
+      title: "go to the market",
+      desc: "to buy t-shirt,you need to go to market"
+    },
+    {
+      sno: 2,
+      title: "go to the gym",
+      desc: "to remain fit and bulky, you need to go to gym"
+    },
+    {
+      sno: 3,
+      title: "go to the shop",
+      desc: "to buy coca-cola,you need to go to shop"
+    }
+  ]
 
   return (
     <>
-      <Header/>
+    <Header title="My Todos List" searchBar={false}/>
+    <Todos todos={todos} onDelete={onDelete}/>
+    <Footer/>
     </>
   );
 }
